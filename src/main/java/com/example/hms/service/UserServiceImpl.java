793047e410e64d5dfde7fc11c3bae4ebfcc3f53b
Sandhiya-S2 <Sandhiya.S2aacf2@cognizant.com>
2026-04-24 14:00:10 +0530

    Update UserServiceImpl.java

--- src/main/java/com/example/hms/service/UserServiceImpl.java
+++ src/main/java/com/example/hms/service/UserServiceImpl.java
@@ -1,22 +1,33 @@
 package com.example.hms.service;
 
+import com.example.hms.dto.ContactQueryRequest;
 import com.example.hms.dto.UserRequest;
 import com.example.hms.dto.UserResponse;
+import com.example.hms.model.ContactQuery;
 import com.example.hms.model.User;
+import com.example.hms.repository.ContactQueryRepository;
 import com.example.hms.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService{
+
     @Autowired
     private UserRepository userRepository;
-    public UserServiceImpl(UserRepository userRepository)
+
+    @Autowired
+    private ContactQueryRepository contactQueryRepository;
+
+    public UserServiceImpl(UserRepository userRepository, ContactQueryRepository contactQueryRepository)
     {
         this.userRepository = userRepository;
+        this.contactQueryRepository = contactQueryRepository;
     }
+
     public UserResponse registerUser(UserRequest userRequest)
     {
         User user = new User();
@@ -30,6 +41,7 @@ public class UserServiceImpl implements UserService{
         User res = userRepository.save(user);
         return mapToResponse(res);
     }
+
     public UserResponse loginUser(User user)
     {
         User checkUser = userRepository.findByEmail(user.getEmail());
@@ -38,6 +50,45 @@ public class UserServiceImpl implements UserService{
             throw new RuntimeException("User Not Found");
         }
         return mapToResponse(checkUser);
+    }
+
+    public List<UserResponse> getAllUsers()
+    {
+        List<User> u=userRepository.findAll();
+        List<UserResponse> urobj=u.stream().map(this::mapToResponse).toList();
+        return urobj;
+    }
+
+    public UserResponse updateUser(Long id,UserRequest userRequest)
+    {
+        User user=userRepository.findById(id).orElseThrow(()->new RuntimeException("User not found"));
+        user.setFullName(userRequest.getFullName());
+        user.setAddress(userRequest.getAddress());
+        user.setCity(userRequest.getCity());
+        user.setGender(userRequest.getGender());
+        user.setEmail(userRequest.getEmail());
+        user.setUpdationDate(LocalDateTime.now());
+        UserResponse userResponse=mapToResponse(user);
+        return userResponse;
+    }
+    public UserResponse getUserById(Long id)
+    {
+        User u=userRepository.findById(id).orElseThrow(()->new RuntimeException("User not found"));
+        UserResponse userResponse=mapToResponse(u);
+        return userResponse;
+    }
+
+    public String saveContactQueries(ContactQueryRequest contactQueryRequest)
+    {
+        ContactQuery contactQuery = new ContactQuery();
+        contactQuery.setFullName(contactQueryRequest.getFullName());
+        contactQuery.setEmail(contactQueryRequest.getEmail());
+        contactQuery.setContactNo(contactQueryRequest.getContactNo());
+        contactQuery.setMessage(contactQueryRequest.getMessage());
+        contactQuery.setPostingDate(LocalDateTime.now());
+        contactQuery.setRead(false);
+        ContactQuery query = contactQueryRepository.save(contactQuery);
+        return "Your information is successfully submitted";
     }
 
     private UserResponse mapToResponse(User user)
@@ -50,29 +101,6 @@ public class UserServiceImpl implements UserService{
         userResponse.setGender(user.getGender());
         userResponse.setRegDate(user.getRegDate());
         userResponse.setUpdationDate(user.getUpdationDate());
-        return userResponse;
-    }
-
-    public List<UserResponse> getAllUsers(){
-        List<User> u=userRepository.findAll();
-        List<UserResponse> urobj=u.stream().map(this::mapToResponse).toList();
-        return urobj;
-    }
-
-    public UserResponse updateUser(Long id,UserRequest userRequest){
-        User user=userRepository.findById(id).orElseThrow(()->new RuntimeException("User not found"));
-        user.setFullName(userRequest.getFullName());
-        user.setAddress(userRequest.getAddress());
-        user.setCity(userRequest.getCity());
-        user.setGender(userRequest.getGender());
-        user.setEmail(userRequest.getEmail());
-        user.setUpdationDate(LocalDateTime.now());
-        UserResponse userResponse=mapToResponse(user);
-        return userResponse;
-    }
-    public UserResponse getUserById(Long id){
-        User u=userRepository.findById(id).orElseThrow(()->new RuntimeException("User not found"));
-        UserResponse userResponse=mapToResponse(u);
         return userResponse;
     }
 }
