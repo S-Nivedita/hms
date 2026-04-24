c8299e2db9c6f94c26eaded06ea7d4ca19143044
Sandhiya-S2 <Sandhiya.S2aacf2@cognizant.com>
2026-04-24 14:14:28 +0530

    Update DoctorService.java

--- src/main/java/com/example/hms/service/DoctorService.java
+++ src/main/java/com/example/hms/service/DoctorService.java
@@ -1,19 +1,15 @@
 package com.example.hms.service;
 
-
 import com.example.hms.dto.ChangePasswordRequest;
-import com.example.hms.dto.UpdateDoctorRequest;
+import com.example.hms.dto.UpdatedDoctorRequest;
 import com.example.hms.dto.UpdatedDoctorResponse;
 
 import java.util.List;
 
 public interface DoctorService {
-
-    UpdatedDoctorResponse updateDoctor(Long id, UpdateDoctorRequest request);
-
-    void changePassword(Long doctorId, ChangePasswordRequest request);
-
-    List<UpdatedDoctorResponse> getAllDoctors();
-    UpdatedDoctorResponse getDoctorById(Long id);
-    UpdatedDoctorResponse addDoctor(UpdateDoctorRequest updateDoctorRequest);
+    public UpdatedDoctorResponse updateDoctor(Long id, UpdatedDoctorRequest request);
+    public void changePassword(Long doctorId, ChangePasswordRequest request);
+    public List<UpdatedDoctorResponse> getAllDoctors();
+    public UpdatedDoctorResponse getDoctorById(Long id);
+    public UpdatedDoctorResponse addDoctor(UpdatedDoctorRequest updateDoctorRequest);
 }
