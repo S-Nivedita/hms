b090a3d4f123341fc1628863b82a171d8997ab46
Sandhiya-S2 <Sandhiya.S2aacf2@cognizant.com>
2026-04-24 13:47:37 +0530

    Update AppointmentRequest.java

--- src/main/java/com/example/hms/dto/AppointmentRequest.java
+++ src/main/java/com/example/hms/dto/AppointmentRequest.java
@@ -4,11 +4,12 @@ import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+
 @Data
 public class AppointmentRequest {
-    private Long userid;
-    private Long doctorid;
+    private Long userId;
+    private Long doctorId;
     private Long consultancyFees;
     private LocalDate appointmentDate;
-    private String appointmentTime;
+    private LocalTime appointmentTime;
 }
