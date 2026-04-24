c1dff9583fc4eddd48c265bdd7b0a9dcb2808345
Sandhiya-S2 <Sandhiya.S2aacf2@cognizant.com>
2026-04-24 13:33:39 +0530

    Create AdminController.java

---
+++ src/main/java/com/example/hms/controller/AdminController.java
@@ -0,0 +1,61 @@
+package com.example.hms.controller;
+
+import com.example.hms.dto.AdminResponse;
+import com.example.hms.dto.ContactQueryResponse;
+import com.example.hms.dto.DashboardResponse;
+import com.example.hms.model.Admin;
+import com.example.hms.model.ContactQuery;
+import com.example.hms.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class AdminController {
+    @Autowired
+    private AdminService adminService;
+
+    public AdminController(AdminService adminService)
+    {
+        this.adminService = adminService;
+    }
+
+    @GetMapping("/admin/dashboard")
+    public ResponseEntity<DashboardResponse> getDashboardDetails()
+    {
+        return ResponseEntity.ok(adminService.getDashboardDetails());
+    }
+
+    @PostMapping("/auth/admin/login")
+    public ResponseEntity<AdminResponse> loginAdmin(@RequestBody Admin admin)
+    {
+        return ResponseEntity.ok(adminService.loginAdmin(admin));
+    }
+
+    @GetMapping("/admin/contact-queries/unread")
+    public ResponseEntity<List<ContactQueryResponse>> getUnreadContactQueries()
+    {
+        return ResponseEntity.ok(adminService.getUnreadContactQueries());
+    }
+
+    @GetMapping("/admin/contact-queries/read")
+    public ResponseEntity<List<ContactQueryResponse>> getReadContactQueries()
+    {
+        return ResponseEntity.ok(adminService.getReadContactQueries());
+    }
+
+    @GetMapping("/admin/contact-queries/{id}")
+    public ResponseEntity<ContactQuery> getContactQueryById(@PathVariable Long id)
+    {
+        return ResponseEntity.ok(adminService.getContactQueryById(id));
+    }
+
+    @PatchMapping("/admin/contact-queries/{id}/remark")
+    public ResponseEntity<String> addAdminRemark(@PathVariable Long id, @RequestBody ContactQuery contactQuery)
+    {
+        return ResponseEntity.ok(adminService.addAdminRemark(id, contactQuery));
+    }
+}
