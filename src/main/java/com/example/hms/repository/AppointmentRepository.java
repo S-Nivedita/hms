69f38431df5683fdcaf59e9b0de6c14b339ececb
Sandhiya-S2 <Sandhiya.S2aacf2@cognizant.com>
2026-04-24 14:08:54 +0530

    Update AppointmentRepository.java

--- src/main/java/com/example/hms/repository/AppointmentRepository.java
+++ src/main/java/com/example/hms/repository/AppointmentRepository.java
@@ -1,7 +1,6 @@
 package com.example.hms.repository;
 
 import com.example.hms.model.Appointment;
-import com.example.hms.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
