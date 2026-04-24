366b9dbfebb6c68df26bfebfddf5279225a0b0b0
Sandhiya-S2 <Sandhiya.S2aacf2@cognizant.com>
2026-04-24 13:58:21 +0530

    Update UserService.java

--- src/main/java/com/example/hms/service/UserService.java
+++ src/main/java/com/example/hms/service/UserService.java
@@ -1,16 +1,17 @@
 package com.example.hms.service;
 
+import com.example.hms.dto.ContactQueryRequest;
 import com.example.hms.dto.UserRequest;
 import com.example.hms.dto.UserResponse;
 import com.example.hms.model.User;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface UserService {
-    UserResponse registerUser(UserRequest userRequest);
-    UserResponse loginUser(User user);
-     List<UserResponse> getAllUsers();
-     UserResponse updateUser(Long id,UserRequest userRequests);
-     UserResponse getUserById(Long id);
+    public UserResponse registerUser(UserRequest userRequest);
+    public UserResponse loginUser(User user);
+    public List<UserResponse> getAllUsers();
+    public UserResponse updateUser(Long id,UserRequest userRequests);
+    public UserResponse getUserById(Long id);
+    public String saveContactQueries(ContactQueryRequest contactQueryRequest);
 }
