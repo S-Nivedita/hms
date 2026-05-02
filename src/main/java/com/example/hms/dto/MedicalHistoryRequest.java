bf8339795d90db82ffac57ba1e6fc24856ec31be
S-Nivedita <Nivedita.S2@cognizant.com>
2026-05-02 20:43:14 +0530

    Updated MedicalHistoryRequest dto

--- src/main/java/com/example/hms/dto/MedicalHistoryRequest.java
+++ src/main/java/com/example/hms/dto/MedicalHistoryRequest.java
@@ -1,12 +1,17 @@
 package com.example.hms.dto;
-
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class MedicalHistoryRequest {
+    @NotBlank(message = "Blood pressure cannot be blank")
     private String bloodPressure;
+    @NotBlank(message = "Blood sugar cannot be blank")
     private String bloodSugar;
+    @NotBlank(message = "Weight cannot be blank")
     private String weight;
+    @NotBlank(message = "Temperature cannot be blank")
     private String temperature;
+    @NotBlank(message = "Medical prescription cannot be blank")
     private String medicalPrescription;
 }
