a7056a4ebe55af17baaf701dabbbbd35302ad708
S-Nivedita <Nivedita.S2@cognizant.com>
2026-05-02 20:35:15 +0530

    Update UserRequest dto

--- src/main/java/com/example/hms/dto/UserRequest.java
+++ src/main/java/com/example/hms/dto/UserRequest.java
@@ -4,11 +4,18 @@ import lombok.Data;
 
 @Data
 public class UserRequest {
+    @NotBlank(message = "Full name cannot be blank")
     private String fullName;
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
     private String email;
+    @NotBlank(message = "Password cannot be blank")
     private String password;
+    @NotBlank(message = "Address cannot be blank")
     private String address;
+    @NotBlank(message = "City cannot be blank")
     private String city;
+    @NotBlank(message = "Gender cannot be blank")
     private String gender;
 }
 
