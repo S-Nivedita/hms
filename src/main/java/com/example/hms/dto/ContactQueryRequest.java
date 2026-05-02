4dd1f16f8f30e63a9bfa06026c28b8d88e2332ff
S-Nivedita <Nivedita.S2@cognizant.com>
2026-05-02 20:38:26 +0530

    Update ContactQueryRequest dto

--- src/main/java/com/example/hms/dto/ContactQueryRequest.java
+++ src/main/java/com/example/hms/dto/ContactQueryRequest.java
@@ -4,8 +4,13 @@ import lombok.Data;
 
 @Data
 public class ContactQueryRequest {
+    @NotBlank(message = "Full name cannot be blank")
     private String fullName;
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
     private String email;
+    @NotNull(message = "Contact number cannot be null")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Contact number must be a valid 10-digit number")
     private Long contactNo;
     private String message;
 }
