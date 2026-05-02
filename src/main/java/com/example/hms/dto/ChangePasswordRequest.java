c0d46dac08918d89c30a153c8054ad11a58907be
S-Nivedita <Nivedita.S2@cognizant.com>
2026-05-02 20:19:22 +0530

    updated ChangePasswordRequest dto

--- src/main/java/com/example/hms/dto/ChangePasswordRequest.java
+++ src/main/java/com/example/hms/dto/ChangePasswordRequest.java
@@ -1,10 +1,17 @@
 package com.example.hms.dto;
 
 import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Data
 public class ChangePasswordRequest {
+
+    @NotBlank(message = "Current password is required")
     private String currentPassword;
+    @NotBlank(message = "New password is required")
+    @Size(min = 10, max = 20, message = "New password must be between 10 and 20 characters")
     private String newPassword;
+    @NotBlank(message = "Confirm password is required")
     private String confirmPassword;
 }
