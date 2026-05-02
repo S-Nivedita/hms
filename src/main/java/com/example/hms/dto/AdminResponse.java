58b1f562da72763bb154f8967f5e6c434001feeb
S-Nivedita <Nivedita.S2@cognizant.com>
2026-05-02 19:59:28 +0530

    Update AdminResponse dto

--- src/main/java/com/example/hms/dto/AdminResponse.java
+++ src/main/java/com/example/hms/dto/AdminResponse.java
@@ -6,7 +6,9 @@ import java.time.LocalDateTime;
 
 @Data
 public class AdminResponse {
+    @NotNull(message = "Id cannot be null")
     private Long id;
+    @NotBlank(message = "Username cannot be blank")
     private String username;
     private LocalDateTime updationDate;
 }
