30c555d46b862279e3d9c105cfdb4e74b010b380
S-Nivedita <Nivedita.S2@cognizant.com>
2026-05-02 19:56:58 +0530

    Update Admin.java

--- src/main/java/com/example/hms/dto/AdminRequest.java
+++ src/main/java/com/example/hms/dto/AdminRequest.java
@@ -6,8 +6,11 @@ import java.time.LocalDateTime;
 
 @Data
 public class AdminRequest {
+    @NotNull(message="Id is required")
     private Long id;
+    @NotNull(message="Name field is required")
     private String username;
+    @NotNull(message="password is required")
     private String password;
     private LocalDateTime updationDate;
 }
