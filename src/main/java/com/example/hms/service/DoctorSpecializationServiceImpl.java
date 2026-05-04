e8e54efee44b6f50e179308b38833a287f8da423
S-Nivedita <Nivedita.S2@cognizant.com>
2026-05-04 16:34:51 +0530

    Refactor DoctorSpecializationServiceImpl methods

--- src/main/java/com/example/hms/service/DoctorSpecializationServiceImpl.java
+++ src/main/java/com/example/hms/service/DoctorSpecializationServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.hms.service;
 
+import com.example.hms.dto.DoctorSpecializationRequest;
+import com.example.hms.dto.DoctorSpecializationResponse;
 import com.example.hms.model.DoctorSpecialization;
 import com.example.hms.repository.DoctorSpecializationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,16 +10,43 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.time.LocalDateTime.now;
+
 @Service
 public class DoctorSpecializationServiceImpl implements DoctorSpecializationService{
     @Autowired
     private DoctorSpecializationRepository doctorSpecializationRepository;
-    
-    public List<String> getAllSpecialization()
-    {
-        List<DoctorSpecialization>l=doctorSpecializationRepository.findAll();
-        List<String> list=new ArrayList<>();
-        l.forEach(i->list.add(i.getSpecialization()));
-        return list;
+    public List<DoctorSpecializationResponse> getAllSpecialization(){
+        List<DoctorSpecialization> l=doctorSpecializationRepository.findAll();
+        return  l.stream().map(this::mapToResponse).toList();
+
+    }
+    public DoctorSpecializationResponse addDoctorsSpecialization(DoctorSpecializationRequest doctorSpecializationRequest){
+        DoctorSpecialization ds=new DoctorSpecialization();
+        ds.setSpecialization(doctorSpecializationRequest.getSpecialization());
+        ds.setCreationDate(now());
+        ds.setUpdationDate(now());
+        doctorSpecializationRepository.save(ds);
+        return mapToResponse(ds);
+    }
+    public String delete(Long id){
+        DoctorSpecialization ds=doctorSpecializationRepository.findById(id).orElseThrow(()->new RuntimeException("Doctor Specialization not found"));
+        doctorSpecializationRepository.delete(ds);
+        return "Doctor Specialization Deleted successfully";
+    }
+    public DoctorSpecializationResponse updateDoctorSpecialization(DoctorSpecializationRequest doctorSpecializationRequest,Long id){
+        DoctorSpecialization ds=doctorSpecializationRepository.findById(id).orElseThrow(()->new RuntimeException("Doctor Specialization not found"));
+        ds.setSpecialization(doctorSpecializationRequest.getSpecialization());
+        ds.setUpdationDate(now());
+        doctorSpecializationRepository.save(ds);
+        return mapToResponse(ds);
+    }
+    public DoctorSpecializationResponse mapToResponse(DoctorSpecialization ds){
+        DoctorSpecializationResponse dsr=new DoctorSpecializationResponse();
+        dsr.setSpecialization(ds.getSpecialization());
+        dsr.setCreationDate(ds.getCreationDate());
+        dsr.setUpdationDate(ds.getUpdationDate());
+        dsr.setId(ds.getId());
+        return dsr;
     }
 }
