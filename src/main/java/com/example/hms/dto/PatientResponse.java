b1aaa528c03f2ea07694e20289978b69491c54d0
Sandhiya-S2 <Sandhiya.S2aacf2@cognizant.com>
2026-05-06 22:59:46 +0530

    Update PatientResponse.java

--- src/main/java/com/example/hms/dto/PatientResponse.java
+++ src/main/java/com/example/hms/dto/PatientResponse.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class PatientResponse {
+    private Long patientId;
     private Long doctorId;
     private String patientName;
     private Long patientContactNo;
