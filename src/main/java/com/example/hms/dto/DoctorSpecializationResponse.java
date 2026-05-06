3bdc39491f2c661a3c58f0c1f9aa48575794885a
Sandhiya-S2 <Sandhiya.S2aacf2@cognizant.com>
2026-05-06 23:00:32 +0530

    Update DoctorSpecializationResponse.java

--- src/main/java/com/example/hms/dto/DoctorSpecializationResponse.java
+++ src/main/java/com/example/hms/dto/DoctorSpecializationResponse.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import java.time.LocalDateTime;
 @Data
 public class DoctorSpecializationResponse {
+    private Long id;
     private String specialization;
     private LocalDateTime creationDate;
     private LocalDateTime updationDate;
